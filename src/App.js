cf8c795a0a303656aa0e9501d3f0ecccff3c20b8
BrownCow371 <jmbrowncow@gmail.com>
2019-01-16 09:38:48 -0800

    remove default app code. add state to store images and grid divs to display images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      clickOrder: [],
+      images: [
+        {
+          id: 0,
+          source: "https://via.placeholder.com/100?text=image0",
+          alt: "Image0",
+          clickCount: 0,
+        },
+        {
+          id: 1,
+          source: "https://via.placeholder.com/100?text=image1",
+          alt: "Image1",
+          clickCount: 0,
+        },
+        {
+          id: 2,
+          source: "https://via.placeholder.com/100?text=image2",
+          alt: "Image2",
+          clickCount: 0,
+        },
+        {
+          id: 3,
+          source: "https://via.placeholder.com/100?text=image3",
+          alt: "Image3",
+          clickCount: 0,
+        },
+        {
+          id: 4,
+          source: "https://via.placeholder.com/100?text=image4",
+          alt: "Image4",
+          clickCount: 0,
+        },
+        {
+          id: 5,
+          source: "https://via.placeholder.com/100?text=image5",
+          alt: "Image5",
+          clickCount: 0,
+        },
+        {
+          id: 6,
+          source: "https://via.placeholder.com/100?text=image6",
+          alt: "Image6",
+          clickCount: 0,
+        },
+        {
+          id: 7,
+          source: "https://via.placeholder.com/100?text=image7",
+          alt: "Image7",
+          clickCount: 0,
+        }
+      ]
+    }
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="wrapper">
+         <div className="grid-container" >
+            {this.state.images.map((image, i) => (
+              <div key={i}><img src={image.source} alt={image.alt} data-id={image.id}/></div>
+            ))}
+        </div>
+        <div>
+            
+        </div>
       </div>
     );
   }
