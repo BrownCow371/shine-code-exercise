3b461ddbb4904e0834ea40b4fb1d9e5f65a05948
BrownCow371 <jmbrowncow@gmail.com>
2019-01-17 15:05:09 -0800

    update upClickCount method to use splice instead of map and set SessionStorage. Add comments for next steps and to explain work.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      clickOrder: [],
+      
       sortedImages: [],
       images: [
         {
@@ -59,35 +59,58 @@ class App extends Component {
       ]
     }
   }
+  // Need to see if we can cleanup state
+  // Also need to persist ClickCounts in SessionStorage for each image and load back into state on mount
+  // Make sure all variables are declared with let or const
+  // update README with takeaways.
+  // Make pretty - can we make images bigger?
+  // can we make log cleaner? - use table?
 
-  splitSessionStorage = (storage) => {
-    return storage.split(",").map(element => parseInt(element, 10))
-  }
-  
+ 
   componentDidMount(){
   
     // set orderClicked based on SessionStorage.order, if set
-    let orderClicked = [];
-    
+    let orderClicked = [];    
     if(sessionStorage.orderClicked){
       orderClicked = this.splitSessionStorage(sessionStorage.orderClicked);
     }
-
+    // set the sortedImage list in sessionStorage and state
+    // allows us to render images in correct order (state) and maintain order for next reload/refresh (sessionStorage)
     let sortedImages = [...orderClicked, ...this.remainingImages(orderClicked)];
-    
     sessionStorage.setItem("sortedImages", sortedImages);
-    sessionStorage.removeItem("orderClicked");
-
     this.setState((state) => {
       return {sortedImages: sortedImages}
-    })
+    });
+
+    // set images in state if sessionSorage contains images
+    if(sessionStorage.images){
+      this.setState((state)=> {
+        return {images: JSON.parse(sessionStorage.images)}
+      })
+    }
+    sessionStorage.setItem("state", JSON.stringify(this.state))
+    console.log("session state", JSON.parse(sessionStorage.state))
+
+    // Clear sessionStorage OrderClicked so it is reset and can be rebuilt for next reload
+    sessionStorage.removeItem("orderClicked");
   }
 
+  // helper function to split Session data into an array
+  splitSessionStorage = (storage) => {
+    return storage.split(",").map(element => parseInt(element, 10));
+  }
+  
+
+  // determine which images have not been clicked in previous page load and set order accordingly
   remainingImages = (order) => {
+    // if the images were previously rearrange per image clicking/page reload
+    // use that previous order to set the order of the images that were not clicked in the last page load
     if (sessionStorage.sortedImages) {
       let sorted = this.splitSessionStorage(sessionStorage.sortedImages);
       let remaining =  sorted.filter((id) => (!order.includes(id)))
       return remaining;
+
+    // if the images have never been clicked on (no sortedImages stored in SessionStorage) use the default order
     } else {
         return (
           this.state.images.filter((element) => (!order.includes(element.id))).map(element => element.id)
@@ -95,29 +118,44 @@ class App extends Component {
       }
   }
 
+// function to update click count
+// Need to update this method to use splice instead of map - may be less costly?
+  // upClickCount = (id) => {
+  //   this.setState((state) => {
+  //     return {
+  //        images: state.images.map((image) => {
+  //          if(image.id === id){
+  //              image.clickCount++;
+  //              return image;
+  //          } else {
+  //            return image;
+  //          } 
+  //        })
+  //       }  
+  //    })
+  //   }
 
+  // function to update click count and set sessionStorage
   upClickCount = (id) => {
     this.setState((state) => {
+      let image = state.images[id];
+      image.clickCount++;
+      let array = state.images
+      array.splice(id, 1, image)
+      sessionStorage.setItem("images", JSON.stringify(array))
       return {
-         images: state.images.map((image) => {
-           if(image.id === id){
-               image.clickCount++;
-               return image;
-           } else {
-             return image;
-           } 
-         })
-       } 
+         images: array
+        }  
      })
-  }
+    }
 
   updateClickOrder = (eventId, id) => {
       let orderClicked = [];
-
+      // set orderClicked to sessionStorage.orderClicked if available
       if (sessionStorage.orderClicked){
         orderClicked = this.splitSessionStorage(sessionStorage.orderClicked)
       }
-
+      // if orderClicked does not already include the id of the clicked image, add it to the array
       if(eventId && !orderClicked.includes(id)){
         orderClicked.push(id); 
         sessionStorage.setItem("orderClicked", orderClicked);
@@ -140,6 +178,8 @@ class App extends Component {
   }
 
 
+  // Create a Grid-cell component, move onClick to compnent, then don't need eventId above
+
   render() { 
 
     return (
@@ -151,7 +191,7 @@ class App extends Component {
         </div>
         <div>
             <Log
-            images={this.state.images}
+              images={this.state.images}
             />
         </div>
       </div>
